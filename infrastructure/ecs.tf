26895369a5c373c4d3c99bb10023d0fc7260b186
Matthias Weiß <mail@matthiasweiss.at>
2024-01-06 22:31:50 +0100

    Disable migrations

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -36,7 +36,8 @@ resource "aws_ecs_task_definition" "laravel_app" {
   container_definitions = jsonencode([
     {
       name  = "aws-ccp-laravel-app"
-      image = "ghcr.io/matthiasweiss/aws-ccp-laravel:main"
+      image = "tutum/hello-world"
+      # image = "ghcr.io/matthiasweiss/aws-ccp-laravel:main"
       portMappings = [
         {
           containerPort = 80
--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -2,6 +2,6 @@ php artisan event:cache
 php artisan route:cache
 php artisan view:cache
 
-php artisan migrate --force
+# php artisan migrate --force
 
 php artisan octane:frankenphp --port 80 --admin-port 2019 --max-requests=1
